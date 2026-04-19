2e30775302b521cbbedab9742589d96146f5cedc
oe-vivo <octavio.vivo@uabc.edu.mx>
2026-04-19 17:51:24 -0400

    test

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//cambio cambio
+//test test
 });
  
 const PORT =5000;
